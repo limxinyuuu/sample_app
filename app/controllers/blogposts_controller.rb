c17068f5756b797eaece0abac4d20117cd500da6
limxinyuuu <limxinyuuu@gmail.com>
2016-08-23 04:02:44 +0800

    Amended well wishes pages

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -2,5 +2,31 @@ class BlogpostsController < ApplicationController
   def index
     #@blogposts = Blogpost.all
     @blogposts = Blogpost.paginate(page: params[:page], per_page: 5)
+    @blogpost = Blogpost.new
   end
+
+  def create
+    @blogpost = Blogpost.new(blogpost_params)
+    if @blogpost.save
+      #flash[:success] = "Message saved!"
+      redirect_to '/blogposts'
+    else
+      #render 'new'
+      redirect_to '/blogposts'
+    end
+  end
+
+  #def new
+  #  @blogpost = Blogpost.new
+  #end
+
+  def destroy
+  end
+
+  private
+
+    def blogpost_params
+      params.require(:blogpost).permit(:name, :content)
+    end
+
 end
--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -1,4 +1,4 @@
 class Blogpost < ApplicationRecord
     validates :name, presence: true
-    validates :content, presence: true
+    validates :content, length: { maximum: 140 }, presence: true
 end
