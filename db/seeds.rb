efb4dddedc539c9945f1d0098e63c1deddfa6b41
limxinyuuu <limxinyuuu@gmail.com>
2016-08-23 01:04:29 +0800

    Added faker and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-users = User.order(:created_at).take(2)
-50.times do
+User.create!(name:  "Example User",
+             email: "example@hotmail.com")
+
+5.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@hotmail.com"
+  User.create!(name:  name,
+               email: email)
+end
+
+users = User.order(:created_at).take(5)
+5.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
