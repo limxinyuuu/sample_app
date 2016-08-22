571c7f69ed5bf5f3d83fedc847b2dbd2164bee16
limxinyuuu <limxinyuuu@gmail.com>
2016-08-22 23:00:44 +0800

    Reverting to previous version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   validates :name, presence: true
+  validates_uniqueness_of :name
   validates :email, presence: true
+  validates_uniqueness_of :email
   #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   #validates :email, presence: true, length: { maximum: 255 },
                   #format: { with: VALID_EMAIL_REGEX }
--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,6 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :microposts,          only: [:create, :destroy]
+
 end
